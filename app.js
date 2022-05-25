6e62d245ea60525aff0577bddeff84729c701195
Abheepsa Pattnaik <abheepsapattnaik@Abheepsas-MacBook-Air.local>
2022-05-25 15:47:16 +0530

    Use express

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+const express = require("express");
 const { sum } = require("./helper");
-const http = require("http");
 
-const server = http.createServer((req, res) => {
-    res.end('Some response');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Response sent ! ')
 })
 
-server.listen(3200);
+app.listen(3200);
 
-const total = sum(11, 27);
-console.log(total);
+
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//     res.end('Some response');
+// })
+// server.listen(3200);
+// const total = sum(11, 27);
+// console.log(total);
