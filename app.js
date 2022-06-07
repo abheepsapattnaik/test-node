035984cd6f85a6f16815709deb079ff053c4a080
Abheepsa Pattnaik <abheepsapattnaik@Abheepsas-MacBook-Air.local>
2022-06-07 18:24:59 +0530

    Add morgan middleware and refactor routes

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
+const { getPosts } = require("./routes/post");
 
-app.get("/", (req, res) => {
-  res.send("Basic api response");
-});
+// const customMiddleware = (req, res, next) => {
+//   console.log("Applying custom middleware");
+//   next();
+// };
+// app.use(customMiddleware);
+
+app.use(morgan("dev")); // middleware
+
+
+app.get("/", getPosts);
 
 const port = 3200;
 app.listen(port, () => {
